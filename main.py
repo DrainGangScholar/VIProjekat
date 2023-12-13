d4b1f94670f405f2993431df8482179de9cef840
DrainGangScholar <veljko.markovic@elfak.rs>
2023-12-13 19:03:49 +0100

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa napreduje aaaaaaaaaaaaaA

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from enum import Enum
+from queue import Queue
 from typing import Optional
+import json
+from collections import deque
 
 class FieldColor(Enum):
     BLACK = "Black"
@@ -24,12 +27,14 @@ class Field():
 
     def add_checker(self,checker)->Optional[CheckerColor]:
         length=len(self.stack)
-        if length+1<8:
-            self.stack.append(checker)
-            return None
-        else:
+        if length ==0:
+            self.stack=checker
+        elif length+len(checker)==8:
             self.stack=[]
-            return checker
+            return True,checker
+        else:
+            self.stack+=checker
+        return False,None
 
     def is_empty(self)->bool:
         if len(self.stack)>0:
@@ -40,7 +45,7 @@ class Field():
         character='.' if self.field_type==field_black else ' '
         matrix = [[character for _ in range(3)] for _ in range(3)]
         for i, checker in enumerate(self.stack):
-            if i < 9:  
+            if i < 8:  
                 row, col = divmod(i, 3)
                 matrix[row][col] = checker.value
         return "\n".join(''.join(row) for row in matrix) + "\n"
@@ -99,6 +104,13 @@ class Player():
         return f"{self.checker_color.value} - Score: {self.score}"
 
 class Game():
+    def __init__(self,board,current_player,player1,player2):
+        self.board=board
+        self.current_player=current_player
+        self.player1=player1
+        self.player2=player2
+        self.winner=None
+
     def __init__(self):
         self.board=None
         self.winner=None
@@ -134,6 +146,34 @@ class Game():
         direction = input("Enter direction (GL, GD, DL, DD): ").upper()
         return row, col, stack_pos, direction
     
+    def bfs(self, start_row, start_col, directions):
+        queue = Queue()
+        visited = set()
+        paths = []
+
+        queue.put([(start_row, start_col, 0)])
+
+        while not queue.empty():
+            path = queue.get()
+            curr_row, curr_col, distance = path[-1]
+
+            if not self.board.fields[curr_row][curr_col].is_empty() and distance > 0:
+                paths.append(path)
+                continue 
+
+            if (curr_row, curr_col) not in visited:
+                visited.add((curr_row, curr_col))
+
+                for _, (dr, dc) in directions.items():
+                    new_row, new_col = curr_row + dr, curr_col + dc
+
+                    if 0 <= new_row < self.board.num_of_fields and 0 <= new_col < self.board.num_of_fields:
+                        queue.put(path + [(new_row, new_col, distance + 1)])
+
+        valid_paths = [path for path in paths if not self.board.fields[path[-1][0]][path[-1][1]].is_empty()]
+
+        return valid_paths 
+
     def is_valid_move(self, start_row, start_col, stack_pos, direction):
         row_index = ord(start_row) - ord('A')
         col_index = start_col - 1
@@ -149,8 +189,8 @@ class Game():
         if len(start_field.stack) == 0:
             return False, "No stack to move."
 
-        if start_field.stack[0] != self.current_player.checker_color:
-            return False, "You do not own the bottom checker in the stack."
+        if start_field.stack[0] != self.current_player.checker_color and start_field.stack[stack_pos] != self.current_player.checker_color:
+            return False, "You do not own the checker you want to move."
 
         dir_offsets = {"GL": (-1, -1), "GD": (-1, 1), "DL": (1, -1), "DD": (1, 1)}
         if direction not in dir_offsets:
@@ -159,6 +199,7 @@ class Game():
         delta_row, delta_col = dir_offsets[direction]
         target_row_index = row_index + delta_row
         target_col_index = col_index + delta_col
+        target_field = self.board.fields[target_row_index][target_col_index]
 
         if not (0 <= target_row_index < self.board.num_of_fields and 0 <= target_col_index < self.board.num_of_fields):
             return False, "Target position is outside the board boundaries."
@@ -166,17 +207,57 @@ class Game():
         if (target_row_index + target_col_index) % 2 != 0:
             return False, "Can only move to dark squares."
 
-        target_field = self.board.fields[target_row_index][target_col_index]
-
         if len(target_field.stack) > 0:  
             if len(start_field.stack) - stack_pos + len(target_field.stack) >= 9:
                 return False, "Cannot form a stack of nine or more."
             
-        if start_field.stack[stack_pos] != self.current_player.checker_color:
-            return False, "It's not your turn."
+        if not target_field.is_empty():
+            if len(target_field.stack) > 0:
+                if (
+                    len(start_field.stack) > stack_pos
+                    and start_field.stack[stack_pos] == self.current_player.checker_color
+                ):
+                    resulting_stack_size = len(target_field.stack) + len(start_field.stack) - stack_pos
 
-        return True, "Valid move."
-    
+                    if resulting_stack_size <= 8:
+                        return True, "Valid move."
+                    else:
+                        return False, "Cannot move the entire stack as it exceeds the maximum size."
+                else:
+                    return False, "Cannot move to a field with an existing stack."
+        else:
+            possible_moves = {"GL": (-1, -1), "GD": (-1, 1), "DL": (1, -1), "DD": (1, 1)}
+
+            temp=possible_moves.pop(direction)
+
+            for _, (move_row, move_col) in possible_moves.items():
+                move_field = self.board.fields[row_index+move_row][col_index+move_col]
+                if not move_field.is_empty():
+                    return False, "There is a non-empty, adjacent field."
+                
+            possible_moves[direction]=temp
+
+            all_paths=self.bfs(row_index,col_index,possible_moves)
+
+            shortest_paths=sorted(all_paths,key=lambda x:len(x))
+            
+            shortest_path=shortest_paths[0]
+
+            print(shortest_path)
+
+            paths = [path for path in shortest_paths if len(path) == len(shortest_path)]
+
+            move_direction=possible_moves[direction]
+            
+            for path in paths:
+                new_row,new_col,_=path[1]
+                test_row=row_index+move_direction[0]
+                test_col=col_index+move_direction[1]
+                if (test_row,test_col)==(new_row,new_col):
+                    return True, "Valid move."
+
+            return False, "There is no valid move."
+
     def won(self):
         num_of_checkers=((self.board_size-2)*self.board_size/2)
         max_score=num_of_checkers/8
@@ -216,22 +297,82 @@ class Game():
         col_index = col - 1
         start_field = self.board.fields[row_index][col_index]
 
-        checker = start_field.stack.pop(stack_pos)
+        stack_to_move = start_field.stack[stack_pos:]
+        start_field.stack = start_field.stack[:stack_pos]
+
         target_row_index, target_col_index = self.calculate_target_position(row_index, col_index, direction)
 
         target_field = self.board.fields[target_row_index][target_col_index]
-        target_field.add_checker(checker)
 
-        #self.current_player.add_score(len(target_field.stack))
+        exceeded,last_checker=target_field.add_checker(stack_to_move)
+        if exceeded:
+            return True, last_checker
+        return False,None
+
+    def add_point(self,last_checker):
+        if self.player1.checker==last_checker:
+            self.player1.score+=1
+            return
+        self.player2.score+=1 
 
     def execute_move(self,move):
         row, col, stack_pos, direction = move
         valid_move,message= self.is_valid_move(row, col, stack_pos, direction)
         if valid_move:
-            self.move(row, col, stack_pos, direction)
+            exceeded, last_checker=self.move(row, col, stack_pos, direction)
+            if exceeded:
+                self.add_point(last_checker)
         else:
             print(message)
         return valid_move
+    
+    def to_json(self):
+        return {
+            "board_size": self.board.num_of_fields,
+            "current_player": self.current_player.checker_color.value,
+            "player1_score": self.player1.score,
+            "player2_score": self.player2.score,
+            "board_fields": [
+                {"type": field.field_type.value, "stack": [checker.value for checker in field.stack]}
+                for row in self.board.fields
+                for field in row
+            ]
+        }
+
+    def from_json(self, data):
+        board_size = data["board_size"]
+        current_player_color = data["current_player"]
+        player1_score = data["player1_score"]
+        player2_score = data["player2_score"]
+        board_fields = data["board_fields"]
+
+        player1 = Player(CheckerColor.X)
+        player1.score=player1_score
+        player2 = Player(CheckerColor.O)
+        player2.score=player2_score
+
+        current_player = player1 if current_player_color == CheckerColor.X.value else player2
+
+        board = Board(board_size)
+
+        fields = []
+        for field_data in board_fields:
+            field_type = FieldColor(field_data["type"])
+            stack = [CheckerColor(checker) for checker in field_data["stack"]]
+            fields.append(Field(field_type, stack))
+
+        board.fields = [fields[i:i + board_size] for i in range(0, len(fields), board_size)]
+
+        return Game(board, current_player, player1, player2)
+
+    def save_to_json(self, filename):
+        with open(filename, "w") as json_file:
+            json.dump(self.to_json(), json_file, indent=2)
+
+    def load_from_json(self, filename):
+        with open(filename, "r") as json_file:
+            data = json.load(json_file)
+        return self.from_json(data)
 
     def __str__(self):
         return f"{self.board}\n" \
@@ -241,7 +382,9 @@ class Game():
 def main():
     game = Game()
     game.start()
+
     while not game.is_over():
+        # game.save_to_json("initial.json")
         print(game)
         move=game.get_move()
         valid=game.execute_move(move)
@@ -249,12 +392,7 @@ def main():
             move=game.get_move()
             valid=game.execute_move(move)
         game.switch_player()
-        # valid_move, error_message = execute_move(game, move)
-
-        # while not valid_move:
-        #     print(error_message)
-        #     move = current_player.get_move()
-        #     #valid_move, error_message = execute_move(game, move)
+    # game.load_from_json("initial.json")
     print(game)
     if game.won():
         print(f"{game.winner} has won!")
