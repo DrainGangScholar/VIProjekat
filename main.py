030a1b65e32b36096baaf7e8642c8b4b8e7f5be7
DrainGangScholar <veljko.markovic@elfak.rs>
2023-12-27 03:11:33 +0100

    mrckaj

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import copy
 from enum import Enum
 from queue import Queue
+import time
 from typing import Optional
-import json
-from collections import deque
+
+DEPTH=3
+MAX_STACK_SIZE=8
 
 class FieldColor(Enum):
     BLACK = "Black"
@@ -30,7 +32,7 @@ class Field():
         length=len(self.stack)
         if length ==0:
             self.stack=checker
-        elif length+len(checker)==8:
+        elif length+len(checker)==MAX_STACK_SIZE:
             self.stack=[]
             return True,checker
         else:
@@ -46,7 +48,7 @@ class Field():
         character='.' if self.field_type==field_black else ' '
         matrix = [[character for _ in range(3)] for _ in range(3)]
         for i, checker in enumerate(self.stack):
-            if i < 8:  
+            if i < MAX_STACK_SIZE:  
                 row, col = divmod(i, 3)
                 matrix[row][col] = checker.value
         return "\n".join(''.join(row) for row in matrix) + "\n"
@@ -111,10 +113,23 @@ class Game():
         self.player1=player1
         self.player2=player2
         self.winner=None
-
+        
     def __init__(self):
         self.board=None
         self.winner=None
+        self.move_history = []
+        self.cache = {}
+        self.curr_moves=[]
+
+
+    def cache_key(self):
+        move_history=tuple(self.move_history)
+        return (
+            self.current_player.checker_color,
+            self.player1.score,
+            self.player2.score,
+            move_history,
+        )
 
     def start(self):
         self.get_board_size()
@@ -124,6 +139,7 @@ class Game():
             self.init(player1,player2)
         else:
             self.init(player2,player1)
+        self.is_valid_count=0
 
     def init(self,player1,player2):
         self.board=Board(self.board_size)
@@ -135,7 +151,7 @@ class Game():
         self.board_size = int(input("Board size: "))
 
     def get_first_player(self):
-        print("Choose who goes first: ")
+        print("Choose which goes first: ")
         print("1.O")
         print("2.X")
         return int(input())
@@ -181,11 +197,25 @@ class Game():
         valid_paths = [path for path in paths if not self.board.fields[path[-1][0]][path[-1][1]].is_empty()]
 
         return valid_paths 
+    
+    def are_all_directions_empty(self, row_index, col_index, directions):
+        for _, (delta_row, delta_col) in directions.items():
+            target_row, target_col = row_index + delta_row, col_index + delta_col
+            
+            move_field=self.bounds_check_and_get_field(target_row,target_col)
+
+            if move_field is not None and not move_field.is_empty():
+                return True  
+        return False  
 
     def is_valid_move(self, start_row, start_col, stack_pos, direction):
+        self.is_valid_count+=1
         row_index = ord(start_row) - ord('A')
         col_index = start_col - 1
-        start_field = self.board.fields[row_index][col_index]
+
+        possible_moves={"GL": (-1, -1), "GD": (-1, 1), "DL": (1, -1), "DD": (1, 1)}
+        if direction not in possible_moves:
+            return False, "Invalid direction."
 
         if not (0 <= row_index < self.board.num_of_fields and 0 <= col_index < self.board.num_of_fields):
             return False, "Move is outside the board boundaries."
@@ -200,11 +230,7 @@ class Game():
         if start_field.stack[stack_pos] != self.current_player.checker_color:
             return False, "You do not own the checker you want to move."
 
-        dir_offsets = {"GL": (-1, -1), "GD": (-1, 1), "DL": (1, -1), "DD": (1, 1)}
-        if direction not in dir_offsets:
-            return False, "Invalid direction."
-
-        delta_row, delta_col = dir_offsets[direction]
+        delta_row, delta_col = possible_moves[direction]
         target_row_index = row_index + delta_row
         target_col_index = col_index + delta_col
         target_field = self.bounds_check_and_get_field(target_row_index,target_col_index)
@@ -223,39 +249,22 @@ class Game():
                 return False, "Cannot form a stack of nine or more."
             
         if not target_field.is_empty():
-            if len(target_field.stack) > 0:
-                if (
-                    len(start_field.stack) > stack_pos
-                ):
-                    if stack_pos > 0 and target_field.stack[-1] >= start_field.stack[stack_pos]:
-                        return False, "Invalid move: The moving checker must be on top of the stack."
-                    num_checkers_to_move = len(start_field.stack[stack_pos:])
+            target_pos=len(target_field.stack)
+            if target_pos < stack_pos or (target_pos == stack_pos and target_pos != 0):
+                return False, "Invalid move: The moving checker must be on top of the stack." #promeni komentar
 
-                    resulting_stack_size = len(target_field.stack) + num_checkers_to_move
+            num_checkers_to_move = len(start_field.stack[stack_pos:])
 
-                    if resulting_stack_size <= 8:
-                        return True, "Valid move."
-                    else:
-                        return False, "Cannot move the stack as it exceeds the maximum size."
-                else:
-                    return False, "Index out of bounds."
+            resulting_stack_size = len(target_field.stack) + num_checkers_to_move
+
+            if resulting_stack_size <= MAX_STACK_SIZE:
+                return True, "Valid move."
+            else:
+                return False, "Cannot move the stack as it exceeds the maximum size."
         else:
-            possible_moves = {"GL": (-1, -1), "GD": (-1, 1), "DL": (1, -1), "DD": (1, 1)}
-
-            temp=possible_moves.pop(direction)
+            if self.are_all_directions_empty(row_index,col_index,possible_moves):
+                return False, "There is a non-empty, adjacent field."
                 
-            for _, (move_row, move_col) in possible_moves.items():
-                new_row, new_col = row_index + move_row, col_index + move_col
-
-                if 0 <= new_row < self.board.num_of_fields and 0 <= new_col < self.board.num_of_fields:
-                    move_field = self.board.fields[new_row][new_col]
-                    if not move_field.is_empty():
-                        return False, "There is a non-empty, adjacent field."
-                else:
-                    return False, "Move is outside the board boundaries."
-                
-            possible_moves[direction]=temp
-
             all_paths=self.bfs(row_index,col_index,possible_moves)
 
             shortest_paths=sorted(all_paths,key=lambda x:len(x))
@@ -277,7 +286,7 @@ class Game():
 
     def won(self):
         num_of_checkers=((self.board_size-2)*self.board_size/2)
-        max_score=num_of_checkers/8
+        max_score=num_of_checkers/MAX_STACK_SIZE
         win_score=(2*max_score)//3
 
         if(self.player1.score>win_score):
@@ -310,11 +319,12 @@ class Game():
         return row_index + delta_row, col_index + delta_col
     
     def add_point(self,last_checker):
-        if self.player0.checker==last_checker:
-            self.player0.score+=1
+        if self.current_player.checker_color==last_checker:
+            print(last_checker)
+            self.current_player.score+=1
             return
-        self.player1.score+=1 
-
+        other=self.get_other_player()
+        other.score+=1 
 
     def move(self, row, col, stack_pos, direction):
         row_index = ord(row) - ord('A')
@@ -337,60 +347,13 @@ class Game():
         row, col, stack_pos, direction = move
         valid_move,message= self.is_valid_move(row, col, stack_pos, direction)
         if valid_move:
+            self.move_history.append(move)
             exceeded, last_checker=self.move(row, col, stack_pos, direction)
             if exceeded:
                 self.add_point(last_checker)
         else:
             print(message)
         return valid_move
-    
-    def to_json(self):
-        return {
-            "board_size": self.board.num_of_fields,
-            "current_player": self.current_player.checker_color.value,
-            "player1_score": self.player1.score,
-            "player2_score": self.player2.score,
-            "board_fields": [
-                {"type": field.field_type.value, "stack": [checker.value for checker in field.stack]}
-                for row in self.board.fields
-                for field in row
-            ]
-        }
-
-    def from_json(self, data):
-        board_size = data["board_size"]
-        current_player_color = data["current_player"]
-        player1_score = data["player1_score"]
-        player2_score = data["player2_score"]
-        board_fields = data["board_fields"]
-
-        player1 = Player(CheckerColor.X)
-        player1.score=player1_score
-        player2 = Player(CheckerColor.O)
-        player2.score=player2_score
-
-        current_player = player1 if current_player_color == CheckerColor.X.value else player2
-
-        board = Board(board_size)
-
-        fields = []
-        for field_data in board_fields:
-            field_type = FieldColor(field_data["type"])
-            stack = [CheckerColor(checker) for checker in field_data["stack"]]
-            fields.append(Field(field_type, stack))
-
-        board.fields = [fields[i:i + board_size] for i in range(0, len(fields), board_size)]
-
-        return Game(board, current_player, player1, player2)
-
-    def save_to_json(self, filename):
-        with open(filename, "w") as json_file:
-            json.dump(self.to_json(), json_file, indent=2)
-
-    def load_from_json(self, filename):
-        with open(filename, "r") as json_file:
-            data = json.load(json_file)
-        return self.from_json(data)
     
     def generate_moves_from_field(self, row, col):
         moves_from_field = []
@@ -408,8 +371,10 @@ class Game():
 
         return moves_from_field
 
-    def generate_all_moves(self):
+    def generate_all_moves(self,color=None):
         all_moves = []
+        if not color:
+            color=self.current_player.checker_color
 
         for row in range(self.board.num_of_fields):
             for col in range(self.board.num_of_fields):
@@ -417,19 +382,165 @@ class Game():
                     field=self.bounds_check_and_get_field(row,col)
                     if not field:
                        continue 
-                    if not field.is_empty() and self.current_player.checker_color in field.stack:
+                    if not field.is_empty() and color in field.stack:
                         moves_from_field = self.generate_moves_from_field(row, col)
                         all_moves.extend(moves_from_field)
         return all_moves
     
     def copy_game(game):
-        new_game = Game()
-        new_game.board = copy.deepcopy(game.board)
-        new_game.current_player = copy.deepcopy(game.current_player)
-        new_game.player1 = copy.deepcopy(game.player1)
-        new_game.player2= copy.deepcopy(game.player2)
-        new_game.winner = game.winner
-        return new_game
+        return copy.deepcopy(game)
+    
+    def min_max_alpha_beta(self, depth, maximizing_player, alpha, beta):
+        key = self.cache_key()
+        if key in self.cache:
+            return self.cache[key]
+
+        if depth == 0 or self.is_over():
+            return self.evaluate_state(), None
+
+        if maximizing_player:
+            max_eval = float('-inf')
+            best_move = None
+            moves = self.generate_all_moves()
+            self.curr_moves=moves
+
+            for move in moves:
+                new_game = self.copy_game()
+                new_game.execute_move(move)
+                eval, _ = new_game.min_max_alpha_beta(depth - 1, False, alpha, beta)
+                if eval > max_eval:
+                    max_eval = eval
+                    best_move = move
+                alpha = max(alpha, max_eval)
+                if beta <= alpha:
+                    break
+            result = max_eval, best_move
+            self.cache[key] = result
+            return result 
+        else:
+            min_eval = float('inf')
+            best_move = None
+            moves = self.generate_all_moves()
+
+            for move in moves:
+                new_game = self.copy_game()
+                new_game.execute_move(move)
+                eval, _ = new_game.min_max_alpha_beta(depth - 1, True, alpha, beta)
+                if eval < min_eval:
+                    min_eval = eval
+                    best_move = move
+                beta = min(beta, min_eval)
+                if beta <= alpha:
+                    break
+            result = min_eval, best_move
+            self.cache[key] = result
+            return result 
+        
+    def make_ai_move(self):
+        depth = DEPTH
+        best_score = float('-inf')
+        best_move = None
+
+        moves = self.generate_all_moves()
+
+        for move in moves:
+            new_game = self.copy_game() 
+            new_game.execute_move(move)
+            score, _ = new_game.min_max_alpha_beta(depth - 1, False, float('-inf'), float('inf'))
+
+            if score > best_score:
+                best_score = score
+                best_move = move
+
+        print(best_move)
+        self.execute_move(best_move)
+
+    def get_other_player(self):
+        if self.current_player==self.player1:
+            return self.player2
+        return self.player1
+    
+    def get_num_stacks(self,color):
+        num=0
+        for row in range(self.board.num_of_fields):
+            for col in range(self.board.num_of_fields):
+                field = self.board.fields[row][col]
+                if field.field_type == field_black and not field.is_empty():
+                    bottom_piece_color = field.stack[0]
+                    if bottom_piece_color == color:
+                        num+=1
+        return num
+    
+    def get_counts_stack(self):
+        color=self.current_player.checker_color
+        stack_count = 0
+        match = 0
+        not_match=0
+        curr_stacks=0
+        other_stacks=0
+
+        for row in range(self.board.num_of_fields):
+            for col in range(self.board.num_of_fields):
+                field = self.board.fields[row][col]
+                
+                if field.field_type == field_black and not field.is_empty() and len(field.stack) > 1:
+                    stack_count += 1
+                    for checker in field.stack:
+                        if checker==color:
+                            match+=1
+                        else:
+                            not_match+=1
+                        if field.stack[0] == color:
+                            curr_stacks+=1
+                        else:
+                            other_stacks+=1
+
+
+        return stack_count,match,not_match, curr_stacks, other_stacks
+
+    def calculate_degree_of_stack_control(self,arg):
+        total_stacks, color_count, not_color_count = arg 
+
+        if total_stacks == 0:
+            return 0.0, 0.0  
+
+        stack_control = color_count / total_stacks
+        not_stack_control = not_color_count / total_stacks
+
+        return stack_control,not_stack_control 
+
+    def evaluate_state(self):
+        score=0
+        curr=self.current_player
+        other=self.get_other_player()
+        #broj mogucih poteza
+        c_count=len(self.curr_moves)
+        p_count=len(self.generate_all_moves(other.checker_color))
+        if c_count>p_count:
+            score+=1
+        elif p_count<p_count:
+            score-=1
+        #ko poseduje vise stackova(prvi element stack-a ai ili player)
+        total,c_checkers,p_checkers,c_stacks,p_stacks=self.get_counts_stack()
+        if c_stacks>p_stacks:
+            score+=1
+        elif c_stacks<p_stacks:
+            score-=1
+        #ko poseduje veci stepen kontrole u stackovima tj. ko u tim stackovima ima ukupno veci broj figura
+        c_degree, p_degree = self.calculate_degree_of_stack_control((total,c_checkers,p_checkers))
+        if c_degree>p_degree:
+            score+=1
+        elif c_degree<p_degree:
+            score-=1
+        #ko ima vise poena kada se napravi potez terminator ili covek
+        c_score=curr.score
+        p_score=other.score
+        if c_score>p_score:
+            score+=1
+        elif c_score<p_score:
+            score-=1
+
+        return score
     
     def __str__(self):
         return f"{self.board}\n" \
@@ -448,6 +559,46 @@ def main():
     print(game)
     if game.won():
         print(f"{game.winner} has won!")
+
+def play_vs_ai():
+    game = Game()
+    game.start()
+
+    while not game.is_over():
+        print(game)
+
+        # Potez igrača
+        valid=False
+        while not valid:
+            move=game.get_move()
+            valid=game.execute_move(move)
+        game.switch_player()
+
+        print(game)
+
+
+        if not game.is_over():
+            game.is_valid_count=0
+            start=time.time()
+            game.make_ai_move()
+            end=time.time()
+            print(f"{game.is_valid_count} moves explored in {end-start} seconds")
+            game.switch_player()
+
+
+    print(game)
+
+    if game.won():
+        print(f"{game.winner} has won!")
+
+def start_game():
+    print("1. Player vs AI")
+    print("2. Player vs Player")
+    flag=input()
+    if(flag=="1"):
+        play_vs_ai()
+    else:
+        main()
     
 if __name__=="__main__":
-    main()
+    start_game()
